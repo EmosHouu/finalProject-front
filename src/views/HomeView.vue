01c736250db4ffe664d8633bf4ee09b36aaa027b
Lilian-Hsiang <lilian4218@gmail.com>
2024-01-30 20:41:46 +0800

    try輪播牆

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
   <HeaderView/>
+  <CarouselView/>
 </template>
 
 <script setup>
   import HeaderView from '@/components/HeaderView.vue';
+  import CarouselView from '@/components/CarouselView.vue';
 </script>
