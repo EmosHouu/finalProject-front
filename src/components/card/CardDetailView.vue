6d92ed34f0410530afae8e39daedbab2699be8b8
Lilian-Hsiang <lilian4218@gmail.com>
2024-02-13 17:56:35 +0800

    做分頁區域卡片

---
+++ src/components/card/CardDetailView.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="cardBody">
+        <div class="card">
+          <v-carousel show-arrows="hover" class="carousel" height="100%" hide-delimiter-background>
+            <v-carousel-item
+              src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+              cover
+            ></v-carousel-item>
+      
+            <v-carousel-item
+              src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
+              cover
+            ></v-carousel-item>
+      
+            <v-carousel-item
+              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+              cover
+            ></v-carousel-item>
+          </v-carousel>
+        </div>
+        <div class="cardBottom">
+            <div class="title">{{title}}</div>
+            <div class="location">
+                <span class="mdi mdi-map-marker">地點：
+                </span>
+                <div>{{location}}</div>
+            </div>
+            <div class="location">
+                <span class="mdi mdi-account">需求人數：
+                </span>
+                <div>{{people}}</div>
+            </div>
+            <div class="location">
+                <span class="mdi mdi-calendar-clock">日期：
+                </span>
+                <div>{{date}}</div>
+            </div>
+            <div><TagView :hashtag="'揪團玩'"></TagView></div>
+          
+        </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import TagView from '../hashtag/TagView.vue';
+  import { defineProps } from 'vue';
+  const props = defineProps(['location', 'title', 'people', 'date']);
+  </script>
+  
+  <style scoped>
+  .card {
+    width: 250px;
+    height: 180px; /* 调整卡片高度，使其适应设计需求 */
+    background-color:white;
+    border-radius:10px 10px 0 0 ;
+  }
+  
+  .carousel {
+    height: 100%; /* 设置輪播牆的高度与卡片相匹配 */
+    width:230px;
+    margin:auto;
+    padding-top:10px;
+  }
+  
+  .cardBottom {
+    width: 250px;
+    height: 150px;
+    background-color: white;
+    border-radius: 0 0 10px 10px;
+  }
+  .cardBody{
+    height:330px;
+    width:250px;
+    border-radius:10px;
+    box-shadow: 0px 3px 2px 2px #e5cfcf;
+    margin-right: 30px;
+  }
+  .location{
+    display:flex;
+    padding-bottom:5px;
+  }
+  .cardBottom{
+    padding-left:10px;
+    padding-top:5px;
+  }
+  </style>
+  
--- src/views/PlayView.vue
+++ src/views/PlayView.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="header">
     <SmallCarouselView :title="'揪團玩'"></SmallCarouselView>
-
+  </div>
+  <div class="body">
+    <SectionView :fill="'#FFD4C0'" :text="'熱門揪團活動'"></SectionView>
+    <SectionView :fill="'#FFB077'" :text="'最新揪團活動'"></SectionView>
   </div>
 </template>
 
 <script setup>
-import SmallCarouselView from '../components/SmallCarouselView.vue'
+import SmallCarouselView from '../components/SmallCarouselView.vue';
+import CardDetailView from '@/components/card/CardDetailView.vue';
+import SectionView from '@/components/SectionView.vue';
 
 </script>
 <style scoped>
   .header{
     width:100%;
-    /* position:relative; */
+  }
+  .body{
+    height:100%;
   }
 
 </style>
